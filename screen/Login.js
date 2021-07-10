d877705fa384c844f04fd96893b757510d98611e
DanujaV <danujagreru@gmail.com>
2021-07-10 20:03:59 +0530

    post data in income

--- screen/Login.js
+++ screen/Login.js
@@ -1,16 +1,38 @@
 /* import 'react-native-gesture-handler'; */
 import React, { Component } from 'react';
-import { Text, Alert, Button, View, StyleSheet } from 'react-native';
+import { Text, Alert, View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
+import { TextArea, Stack, Center, NativeBaseProvider, Button } from "native-base"
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
+      email: '',
       username: '',
       password: '',
     };
+  }
+
+  abc() {
+    console.log('hi')
+  }
+
+  saveLogin() {
+    fetch('http://localhost:9000/expense/', {
+  method: 'POST',
+  body: JSON.stringify({
+    email: this.state.email,
+    name: this.state.name,
+    password: this.state.password,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
   }
 
   go = () => {
@@ -32,14 +54,22 @@ export default class App extends Component {
 
   render() {
     return (
+      <NativeBaseProvider>
       <View style={styles.container}>
-      <Text style={styles.inputext}>Sample Login Form</Text>
         <TextInput
-          value={this.state.username}
-          onChangeText={(username) => this.setState({ username })}
+          value={this.state.email}
+          onChangeText={(email) => this.setState({ email })}
            label='Email'
           style={styles.input}
         />
+
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+           label='User name'
+          style={styles.input}
+        />
+
         <TextInput
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
@@ -47,15 +77,36 @@ export default class App extends Component {
           secureTextEntry={true}
           style={styles.input}
         />
-        
-        <Button
-          title={'Login'}
-          style={styles.input}
-          onPress={()=>{
-            this.props.navigation.replace('Dashboard')
-        }}
-        />
+
+         <Button
+                style={styles.button}
+                size="lg"
+               /*  variant="outline" */
+                colorScheme="primary"
+                onPress={()=>{
+                  fetch('http://192.168.1.103:9000/user', {
+                    method: 'POST',
+                    headers: {
+                      Accept: 'application/json',
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                      email: this.state.email,
+                      name: this.state.username,
+                      password: this.state.password,
+                    })
+                    
+                  })
+                    .then((response) => response.json())
+                    .then((json) => console.log(json));
+                  
+                  this.props.navigation.replace('Dashboard')
+              }}
+              >
+                Get in
+              </Button>
       </View>
+      </NativeBaseProvider>
     );
   }
 }
@@ -65,9 +116,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#00FFFF',
+    backgroundColor: 'white'
+  
   },
-  input: {
+
+
+   input: {
     width: 200,
     height: 44,
     padding: 10,
@@ -84,5 +138,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black',
     marginBottom: 10,
-  },
+  }, 
 });
